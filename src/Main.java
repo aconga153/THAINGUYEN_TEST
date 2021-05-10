d0d32a547273de02ac23cfb4fe84587b9e78d80f
Admin <wardaddy351@gmail.com>
2021-05-10 11:08:01 +0700

    add thread

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("function 2");
+
+        //thread 1
+    new Thread(()-> {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("a ");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }).start();
+
+        //thread 2
+        new Thread(()-> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("b ");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+        System.out.println("main end");
     }
 }
